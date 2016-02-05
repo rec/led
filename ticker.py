b775f71282d1043e99266778478f539f73d0bdcb
Tom Ritchford <tom@swirly.com>
2016-02-05 13:45:59 -0500

    better animation

--- ticker.py
+++ ticker.py
@@ -3,24 +3,33 @@ from bibliopixel import animation, colors
 class Ticker(animation.BaseStripAnim):
     def __init__(self, led):
         super(Ticker, self).__init__(led)
-        self._internalDelay = 500
-        self.colors =  [colors.Red, colors.Green, colors.Blue, colors.White]
+        self._internalDelay = 4
+        self.running = True
+        self.last_scroll = 0
+        self.speed_in_steps = 125
+        self.direction = 1
+
+    def scroll(self, steps):
+        # https://stackoverflow.com/questions/9457832/python-list-rotation
+        led = self._led
+        steps = steps % led.numLEDs
+        led.buffer = led.buffer[-3 * steps:] + led.buffer[:-3 * steps]
 
     def step(self, amt = 1):
+        if not self._step:
+            self.setme()
 
+        self._step += 1
+        if self.last_scroll >= self.speed_in_steps:
+            self.last_scroll = 0
+            self.scroll(self.direction)
+        else:
+            self.last_scroll += 1
+
+    def setme(self):
         self._led.set(0, colors.Red)
         self._led.set(1, colors.Green)
         self._led.set(2, colors.Green)
         self._led.set(3, colors.Blue)
         self._led.set(4, colors.Blue)
         self._led.set(5, colors.Blue)
-
-        color =  self._step % 4
-        self._led.fill(self.colors[color], 7, 9)
-
-        self._step += 1
-        if self._internalDelay < 100:
-            print('here!')
-            self._internalDelay = 100
-        elif self._internalDelay > 100:
-            self._internalDelay -= 11
