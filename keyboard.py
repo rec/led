e8bb646cbd6f7fd6e305dd109d14b13c450314da
Tom Ritchford <tom@swirly.com>
2016-02-04 20:03:09 -0500

    Componentize keyboard.

--- keyboard.py
+++ keyboard.py
@@ -1,23 +1,29 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
 # From https://stackoverflow.com/questions/9865446
 
 import termios, fcntl, sys, os
 
-fd = sys.stdin.fileno()
+def keyboard(callback):
+    fd = sys.stdin.fileno()
 
-oldterm = termios.tcgetattr(fd)
-newattr = termios.tcgetattr(fd)
-newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
-termios.tcsetattr(fd, termios.TCSANOW, newattr)
+    oldterm = termios.tcgetattr(fd)
+    newattr = termios.tcgetattr(fd)
+    newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
+    termios.tcsetattr(fd, termios.TCSANOW, newattr)
 
-oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
-fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
+    oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
+    fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
 
-try:
-    while True:
-        try:
-            c = sys.stdin.read(1)
-            print "Got character", repr(c)
-        except IOError: pass
-finally:
-    termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
-    fcntl.fcntl(fd, fcntl.F_SETFL, oldflags)
+    try:
+        while True:
+            try:
+                callback(sys.stdin.read(1))
+            except IOError:
+                pass
+    finally:
+        termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
+        fcntl.fcntl(fd, fcntl.F_SETFL, oldflags)
+
+if __name__ == '__main__':
+    keyboard(lambda c: print('Keystroke '+ c))
