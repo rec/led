91231214f9f63cdd3dc94f53b5f6eb68005bcfd7
Tom Ritchford <tom@swirly.com>
2016-02-05 15:04:21 -0500

    Add reverse, and more work on selected.

--- ticker.py
+++ ticker.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 from bibliopixel import animation, colors
 
 class Ticker(animation.BaseStripAnim):
@@ -8,6 +10,8 @@ class Ticker(animation.BaseStripAnim):
         self.last_scroll = 0
         self.period_in_steps = 125
         self.direction = 1
+        self.selected = 0
+        self.flash_selected = False
 
     def scroll(self, steps):
         led = self._led
@@ -18,6 +22,10 @@ class Ticker(animation.BaseStripAnim):
     def toggle_pause(self):
         self.running = not self.running
         print('Running.' if self.running else 'Paused.')
+
+    def increment_selected(self, increment=1):
+        self.selected = (self.selected + increment) % self._led.numLEDs
+        print('Selected is now', self.selected)
 
     def step(self, amt = 1):
         if not self._step:
@@ -33,21 +41,21 @@ class Ticker(animation.BaseStripAnim):
             else:
                 self.last_scroll += 1
 
+    def reverse(self):
+        self.direction = -self.direction
+
     def change_speed(self, increment):
-        if self.direction < 0:
-            increment = -increment
-        if not self.period_in_steps:
-            self.direction = abs(increment) / increment
+        self.period_in_steps *= 1.0 + increment / 100.0
+        if self.period_in_steps < 1:
             self.period_in_steps = 1
-        else:
-            self.period_in_steps *= 1.0 + increment / 100.0
-            if self.period_in_steps < 1:
-                self.period_in_steps = 0
-        print 'speed is now', self.period_in_steps
+
+        print('speed is now',
+              (1000 / self._internalDelay) / self.period_in_steps,
+              'LEDs per second')
 
     def change_color(self, color_index, increment):
-        self._led.buffer[color_index] = (
-            (self._led.buffer[color_index] + increment) % 256)
+        i = color_index + 3 * self.selected
+        self._led.buffer[i] = (self._led.buffer[i] + increment) % 256
 
     def setme(self):
         self._led.set(0, colors.Red)
