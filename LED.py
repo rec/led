2c57d98251a21055eeff9f0e6a3ca077fb9b98d7
Tom Ritchford <tom@swirly.com>
2016-02-09 15:39:54 -0500

    Fix blackout.

--- LED.py
+++ LED.py
@@ -10,11 +10,13 @@ import scroller
 
 class LED(bibliopixel.animation.BaseStripAnim):
     def __init__(self, internal_delay=4, number=80):
+        self.number = number
         driver = DriverSerial(num=number, type=LEDTYPE.LPD8806)
         self.led = bibliopixel.led.LEDStrip(driver)
         super(LED, self).__init__(self.led)
         self.led._internalDelay = internal_delay
         self.scroller = scroller.Scroller(self.led)
+        self.blacked_out = False
 
     def step(self, amt=1):
         if not self._step:
@@ -27,9 +29,22 @@ class LED(bibliopixel.animation.BaseStripAnim):
         self.scroller.scroll(steps)
 
     def randomize(self, randomizer=lambda: random.randint(0, 255)):
+        self.blacked_out and self.blackout()
         print('randomizing')
-        for i in xrange(len(self.led.buffer)):
+        for i in xrange(3 * self.number):
             self.led.buffer[i] = int(max(0, min(255, randomizer())))
+
+    def blackout(self):
+        if not self.blacked_out:
+            self.saved = self.led.buffer
+            self.clear()
+        else:
+            self.led.buffer = self.saved
+        self.blacked_out = not self.blacked_out
+
+    def clear(self):
+        self.blacked_out and self.blackout()
+        self.led.buffer = 3 * self.number * [0]
 
     def exit(self):
         self.led.all_off()
