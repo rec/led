825c7458da3b7c7f3f1aa58f8d44801a7deea314
Tom Ritchford <tom@swirly.com>
2016-02-12 19:46:09 -0500

    Experimenting with Curses.

--- Curses.py
+++ Curses.py
@@ -2,6 +2,31 @@ from __future__ import print_function
 
 import curses
 
+class Color(object):
+    COLORS = []
+
+    @staticmethod
+    def add_color(color):
+        Color.COUNT += 1
+        return Color.COUNT
+
+    def __init__(self,
+                 foreground=curses.COLOR_BLACK, background=curses.COLOR_WHITE):
+        self.index = Color.next_index()
+        self.set(foreground, background)
+
+    def set(self, foreground=None, background=None):
+        self.foreground = foreground or self.foreground
+        self.background = background or self.background
+        curses.init_pair(self.index, self.foreground, self.background)
+
+
+class Style(object):
+    def __init__(self, color, flags=None):
+        self.color = color
+        self.flags = flags
+
+
 def main(screen):
     screen.clear()
     screen_y, screen_x = screen.getmaxyx()
